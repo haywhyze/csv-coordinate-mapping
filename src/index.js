d9e0645610ad24b30f9c95bd0bc48bca9ac9e8d2
Yusuf Abdulkarim <haywhyze@gmail.com>
2021-04-28 10:12:07 +0100

    show coordinates on single

--- src/index.js
+++ src/index.js
@@ -39,193 +39,198 @@ function App() {
   };
 
   return (
-    <div className='App'>
-      <div className='data-view'>
-        <div
-          className='no-print'
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-          }}
-        >
-          <CSVReader
-            parserOptions={{ header: true }}
-            onFileLoaded={(data, fileInfo) => {
-              const formatted = data
-                .map((e) => ({
-                  lat: Number(e.latitude || e.Latitude),
-                  lng: Number(e.longitude || e.Longitude),
-                }))
-                .filter((e) => e.lng && e.lat);
-              setData(formatted);
-              setFileName(fileInfo.name.split('.')[0]);
-            }}
-          />
+    <div>
+      <h1>Tellagri Map Plotter</h1>
+      <div className='App'>
+        <div className='data-view'>
           <div
+            className='no-print'
             style={{
-              paddingTop: '1rem',
               display: 'flex',
-              justifyContent: 'space-between',
+              flexDirection: 'column',
             }}
           >
-            <button
-              onClick={() => {
-                if (!data[0]) return;
-                setPaths([...paths, data]);
-                setCenter(data[0]);
-                const detail = {};
-                let test = data.map(
-                  (e) => new google.maps.LatLng(e.lat, e.lng)
-                );
-                Geocode.fromLatLng(data[1].lat, data[1].lng).then(
-                  (response) => {
-                    const address = response.results[0];
-                    detail.lgaType =
-                      address.address_components.length > 4 ? 'LGA' : 'City';
-                    detail.state =
-                      address.address_components[
-                        address.address_components.length - 2
-                      ].long_name;
-                    detail.lga =
-                      address.address_components[
-                        address.address_components.length - 3
-                      ].long_name;
-                    detail.size =
-                      parseFloat(
-                        (
-                          google.maps.geometry.spherical.computeArea(test) /
-                          10000
-                        ).toFixed(4)
-                      ) + ' Ha';
-                    detail.fileName = fileName;
-                    setDetails([...details, detail]);
-                  },
-                  (error) => {
-                    console.error(error);
-                  }
-                );
+            <CSVReader
+              parserOptions={{ header: true }}
+              onFileLoaded={(data, fileInfo) => {
+                const formatted = data
+                  .map((e) => ({
+                    lat: Number(e.latitude || e.Latitude),
+                    lng: Number(e.longitude || e.Longitude),
+                  }))
+                  .filter((e) => e.lng && e.lat);
+                setData(formatted);
+                setFileName(fileInfo.name.split('.')[0]);
+              }}
+            />
+            <div
+              style={{
+                paddingTop: '1rem',
+                display: 'flex',
+                justifyContent: 'space-between',
               }}
             >
-              Upload
-            </button>
-            {document.querySelector('#react-csv-reader-input') &&
-            document.querySelector('#react-csv-reader-input').value ? (
               <button
                 onClick={() => {
-                  setPaths([]);
-                  setData([]);
-                  setDetails([]);
-                  setCenter({ lat: 6.439744, lng: 3.456023 });
-                  document.querySelector(
-                    '#react-csv-reader-input'
-                  ).value = null;
+                  if (!data[0]) return;
+                  setPaths([...paths, data]);
+                  setCenter(data[0]);
+                  const detail = {};
+                  let test = data.map(
+                    (e) => new google.maps.LatLng(e.lat, e.lng)
+                  );
+                  Geocode.fromLatLng(data[1].lat, data[1].lng).then(
+                    (response) => {
+                      const address = response.results[0];
+                      detail.lgaType =
+                        address.address_components.length > 4 ? 'LGA' : 'City';
+                      detail.state =
+                        address.address_components[
+                          address.address_components.length - 2
+                        ].long_name;
+                      detail.lga =
+                        address.address_components[
+                          address.address_components.length - 3
+                        ].long_name;
+                      detail.size =
+                        parseFloat(
+                          (
+                            google.maps.geometry.spherical.computeArea(test) /
+                            10000
+                          ).toFixed(4)
+                        ) + ' Ha';
+                      detail.fileName = fileName;
+                      setDetails([...details, detail]);
+                    },
+                    (error) => {
+                      console.error(error);
+                    }
+                  );
                 }}
               >
-                Clear
+                Upload
               </button>
-            ) : null}
-          </div>
-        </div>
-        {paths.length ? (
-          <>
-            {details.map((e, i) => (
-              <div
-                key={Math.random()}
-                className='details'
-                style={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  textAlign: 'left',
-                  lineHeight: 2,
-                }}
-              >
-                <span>
-                  <strong>State:</strong> {e.state}
-                </span>
-                <span>
-                  <strong>{e.lgaType === 'City' ? 'City' : 'LGA'}:</strong>{' '}
-                  {e.lga}
-                </span>
-                <span>
-                  <strong>Land Size:</strong> {e.size}
-                </span>
-                <small>{e.fileName}</small>
+              {document.querySelector('#react-csv-reader-input') &&
+              document.querySelector('#react-csv-reader-input').value ? (
                 <button
-                  className='no-print'
-                  onClick={() => setCenter(paths[i][0])}
+                  onClick={() => {
+                    setPaths([]);
+                    setData([]);
+                    setDetails([]);
+                    setCenter({ lat: 6.439744, lng: 3.456023 });
+                    document.querySelector(
+                      '#react-csv-reader-input'
+                    ).value = null;
+                  }}
                 >
-                  set as map center
+                  Clear
                 </button>
-              </div>
-            ))}
-            {/* <table style={{ marginTop: "2rem" }}>
-              <thead>
-                <tr>
-                  <th style={{ textAlign: "center" }} colSpan="2">
-                    Coordinates
-                  </th>
-                </tr>
-                <tr>
-                  <th>Latitude</th>
-                  <th>Longitude</th>
-                </tr>
-              </thead>
-              <tbody>
-                {paths.map((e) => (
-                  <tr key={Math.random()}>
-                    <td>{e.lat}</td>
-                    <td>{e.lng}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table> */}
-          </>
-        ) : null}
-      </div>
-      <div className='data'>
-        <div className='App-map'>
-          {paths && (
-            <GoogleMap
-              id='marker-example'
-              mapContainerStyle={mapContainerStyle}
-              zoom={16}
-              center={center}
-            >
-              {paths.map((e) => (
-                <Polygon key={Math.random()} paths={e} options={options} />
+              ) : null}
+            </div>
+          </div>
+          {paths.length ? (
+            <>
+              {details.map((e, i) => (
+                <div
+                  key={Math.random()}
+                  className='details'
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    textAlign: 'left',
+                    lineHeight: 2,
+                  }}
+                >
+                  <span>
+                    <strong>State:</strong> {e.state}
+                  </span>
+                  <span>
+                    <strong>{e.lgaType === 'City' ? 'City' : 'LGA'}:</strong>{' '}
+                    {e.lga}
+                  </span>
+                  <span>
+                    <strong>Land Size:</strong> {e.size}
+                  </span>
+                  <small className='no-print'>{e.fileName}</small>
+                  <button
+                    className='no-print'
+                    onClick={() => setCenter(paths[i][0])}
+                  >
+                    set as map center
+                  </button>
+                </div>
               ))}
-
-              {showPath &&
-                paths.map((path) =>
-                  path.map((e, i) => (
-                    <Marker
-                      key={Math.random()}
-                      position={e}
-                      label={(i + 1).toString()}
-                    />
-                  ))
-                )}
-            </GoogleMap>
-          )}
+              {paths.length === 1 ? (
+                <table style={{ marginTop: '2rem' }}>
+                  <thead>
+                    <tr>
+                      <th style={{ textAlign: 'center' }} colSpan='2'>
+                        Coordinates
+                      </th>
+                    </tr>
+                    <tr>
+                      <th>Latitude</th>
+                      <th>Longitude</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {paths[0].map((e) => (
+                      <tr key={Math.random()}>
+                        <td>{e.lat}</td>
+                        <td>{e.lng}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              ) : null}
+            </>
+          ) : null}
         </div>
-        {paths.length ? (
-          <>
-            <button
-              className='no-print'
-              onClick={() => setShowPath(!showPath)}
-              style={{ marginTop: '3rem' }}
-            >
-              {!showPath ? 'Show Path' : 'Hide Path'}
-            </button>
-            <button
-              className='no-print'
-              onClick={() => window.print()}
-              style={{ marginTop: '3rem', marginLeft: '2rem' }}
-            >
-              Download
-            </button>
-          </>
-        ) : null}
+        <div className='data'>
+          <div className='App-map'>
+            {paths && (
+              <GoogleMap
+                id='marker-example'
+                mapContainerStyle={mapContainerStyle}
+                zoom={16}
+                center={center}
+              >
+                {paths.map((e) => (
+                  <Polygon key={Math.random()} paths={e} options={options} />
+                ))}
+
+                {showPath &&
+                  paths.map((path) =>
+                    path.map((e, i) => (
+                      <Marker
+                        key={Math.random()}
+                        position={e}
+                        label={(i + 1).toString()}
+                      />
+                    ))
+                  )}
+              </GoogleMap>
+            )}
+          </div>
+          {paths.length ? (
+            <>
+              <button
+                className='no-print'
+                onClick={() => setShowPath(!showPath)}
+                style={{ marginTop: '3rem' }}
+              >
+                {!showPath ? 'Show Path' : 'Hide Path'}
+              </button>
+              <button
+                className='no-print'
+                onClick={() => window.print()}
+                style={{ marginTop: '3rem', marginLeft: '2rem' }}
+              >
+                Download
+              </button>
+            </>
+          ) : null}
+        </div>
       </div>
     </div>
   );
